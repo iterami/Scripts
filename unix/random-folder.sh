1127dfa0edf6613f5c6b3e3d160022a1951bc687
Jan Horava <czechinthemail@gmail.com>
2014-09-30 23:10:36 -0700

    fixed unix/random-folder.sh

--- unix/random-folder.sh
+++ unix/random-folder.sh
@@ -7,4 +7,4 @@
 # Example: sh random-folder.sh target/folder/
 
 # Return a random folder from the target folder.
-find $1 -maxdepth 1 -mindepth 1 -exec basename {} \; -type d | shuf -n1
+find $1 -maxdepth 1 -mindepth 1 -type d -exec basename {} \; | shuf -n1
