ef30d16a195287fe32fe6a2cb1c4bc947fd0aab5
Jan Horava <czechinthemail@gmail.com>
2015-01-06 02:51:41 -0800

    added git/create-past-commit.sh, improved example for git/commit-date-change.sh

--- git/create-past-commit.sh
+++ git/create-past-commit.sh
@@ -4,10 +4,10 @@
 #   $1: Relative path to the folder in which the
 #         repository containing the commit you wish
 #         to modify is located.
-#   $2: The hash of the commit you wish to modify.
+#   $2: Commit message.
 #   $3: The date you wish to use, in timestamp format.
 #
-# Example: sh commit-date-change.sh repositories/repository 92bbc4d9adb7aa284f5b7dd8b666be8d546c51fe 1408708800
+# Example: sh create-past-commit.sh repositories/repository 'This is a commit message!' 1408708800
 
 # Check if at least 3 arguments were passed.
 if [ $# -lt 3 ]
@@ -16,12 +16,25 @@ then
     exit 2
 fi
 
-# Navigate to the target folder name.
+# Navigate to the repository root folder.
 cd $1
 
+# Add all changes.
+git add .
+git add . -u
+
+# Commit the changes.
+git commit -m "$2"
+
+# Get the hash of the committed commit.
+hash=`git log -1 --format=\'%H\'`
+
 # Modify the date of the specified commit.
-git filter-branch -f --env-filter 'if [ $GIT_COMMIT = '$2' ]
+git filter-branch -f --env-filter 'if [ $GIT_COMMIT = '$hash' ]
     then
         export GIT_AUTHOR_DATE='$3'
         export GIT_COMMITTER_DATE='$3'
     fi'
+
+# Push the changes.
+git push
