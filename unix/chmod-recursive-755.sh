09bb28dc28a6baf1fc9f1360b6a41eb152a02e45
Jan Horava <czechinthemail@gmail.com>
2015-03-22 12:54:51 -0700

    added unix/chmod-recursive-755.sh

---
+++ unix/chmod-recursive-755.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# Required arguments:
+#   $1: Relative path to the directory
+#         that will be modified.
+#
+# Example: sh chmod-recursive-775.sh target/directory/
+
+# Check if at least 1 argument was passed.
+if [ $# -lt 1 ]
+then
+    echo 'Missing argument.'
+    exit 2
+fi
+
+# Navigate to the target directory.
+cd $1
+
+# Recursively modify permissions.
+chmod -R 755 .
