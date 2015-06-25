d6986415c5aff7797cee380e280e2d9d0a3f511b
Jan Horava <czechinthemail@gmail.com>
2015-06-24 19:18:43 -0700

    added ubuntu/remove-overlay-scrollbars.sh

---
+++ ubuntu/remove-overlay-scrollbars.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# No arguments.
+
+# Ubuntu 12.04
+gsettings set org.gnome.desktop.interface ubuntu-overlay-scrollbars false
+# Ubuntu 14.04
+gsettings set com.canonical.desktop.interface scrollbar-mode normal
