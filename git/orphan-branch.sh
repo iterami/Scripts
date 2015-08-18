abab4f11128b926ac1c2ebbc5e6953562890eae4
Jan Horava <czechinthemail@gmail.com>
2015-08-17 19:37:58 -0700

    added git/orphan-branch.sh

---
+++ git/orphan-branch.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+# Required arguments:
+#   $1: Relative path to the root directory of the
+#         repository you wish to modify.
+#   $2: Name of the orphan branch to create.
+#
+# Example: sh orphan-branch.sh repositories/repository gh-pages
+
+# Check if at least 2 arguments were passed.
+if [ $# -lt 2 ]
+then
+    echo 'Missing arguments.'
+    exit 2
+fi
+
+# Navigate to the repository root directory.
+cd $1
+
+# Create the orphan branch.
+git checkout --orphan $2
+
+# Force the branch to be empty.
+git rm rf .
