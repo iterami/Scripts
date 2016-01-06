d934296237cd2122613d825a262dae803b1f56f3
Jan Horava <czechinthemail@gmail.com>
2016-01-05 20:20:32 -0800

    added git/grep-cloned-repo.sh

---
+++ git/grep-cloned-repo.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# Required arguments:
+#   $1: Relative path to the root directory of the
+#         repository you wish to modify.
+#   $2: String to grep.
+#
+# Example: sh grep-cloned-repo.sh repositories/repository "string"
+
+# Check if at least 2 arguments were passed.
+if [ $# -lt 2 ]
+then
+    echo 'Missing arguments: path string'
+    exit 2
+fi
+
+# Navigate to the repository root directory.
+cd $1
+
+# grep.
+grep -l -r --exclude-dir=".git" $2 .
