7e57dd56f5865bc007699bb51a629ff7d72291d7
honzi <czechinthemail@gmail.com>
2016-11-09 09:46:20 -0800

    added git/clear-commit-history.sh

---
+++ git/clear-commit-history.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+# Required arguments:
+#   $1: Relative path to the root directory of the
+#         repository you wish to modify.
+#   $2: Name of the branch you wish to use.
+#   $3: Commit message.
+#
+# Example: sh clear-commit-history.sh master "This is a commit message!"
+
+# Check if at least 3 arguments were passed.
+if [ $# -lt 3 ]
+then
+    echo 'Missing arguments: path branch commit-message'
+    exit 2
+fi
+
+# Navigate to the repository root directory.
+cd $1
+
+# Checkout to a temporary branch.
+git checkout --orphan temporary_branch
+
+# Add all files.
+git add .
+git add . -u
+
+# Commit the files.
+git commit -m "$3"
+
+# Delete the selected branch.
+git branch -D $2
+
+# Rename temporary branch.
+git branch -m $2
+
+# Force push.
+git push -f origin $2
