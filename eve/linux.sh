ba5c50352eed0a1fbdc23ded925436cd7af34bb8
Jan Hořava <czechinthemail@gmail.com>
2018-02-18 11:24:37 -0800

    added eve script

---
+++ eve/linux.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# No arguments.
+
+# Download launcher.
+wget https://binaries.eveonline.com/evelauncher-1104891.tar.gz
+
+# Extract launcher files.
+tar -xvf evelauncher-1104891.tar.gz
+
+# cd to launcher directory.
+cd evelauncher
+
+# Make evelauncher.sh executable.
+chmod u+x evelauncher.sh
