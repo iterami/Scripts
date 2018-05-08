89ef983caba76d501fd89a3c2a76e0cf9359ead0
Jan Hořava <czechinthemail@gmail.com>
2018-05-07 23:51:25 -0700

    added mode to xrandr scale script

--- ubuntu/xrandr-scale.sh
+++ ubuntu/xrandr-scale.sh
@@ -1,19 +1,21 @@
 #!/bin/sh
 
 # Required arguments:
-#   $1: Scale x.
-#   $2: Scale y.
-#   $3: Resolution x.
-#   $4: Resolution y.
+#   $1: Mode x.
+#   $2: Mode y.
+#   #3: Panning x.
+#   #4: Panning y.
+#   $5: Scale x.
+#   $6: Scale y.
 #
-# Example: sh xrandr-scale.sh 1.25 1.25 1600 900
+# Example: sh xrandr-scale.sh 1600 900 1920 1080 1.2 1.2
 
-# Check if at least 3 arguments were passed.
-if [ $# -lt 4 ]
+# Check if at least 6 arguments were passed.
+if [ $# -lt 6 ]
 then
-    echo 'Missing arguments: scale_x scale_y resolution_x resolution_y'
+    echo 'Missing arguments: mode_x mode_y panning_x panning_y scale_x scale_y
     exit 2
 fi
 
 # Scale.
-xrandr --output eDP-1 --scale $1x$2 --panning $3x$4+0+0
+xrandr --output eDP-1 --mode $1x$2 --panning $3x$4 --scale $5x$6
