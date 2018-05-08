91f168394392ca948c8bfc66ed07652fb03ae39b
Jan Hořava <czechinthemail@gmail.com>
2018-05-08 14:17:02 -0700

    added output arg

--- ubuntu/xrandr-scale.sh
+++ ubuntu/xrandr-scale.sh
@@ -1,21 +1,22 @@
 #!/bin/sh
 
 # Required arguments:
-#   $1: Mode x.
-#   $2: Mode y.
-#   #3: Panning x.
-#   #4: Panning y.
-#   $5: Scale x.
-#   $6: Scale y.
+#   $1: Output.
+#   $2: Mode x.
+#   $3: Mode y.
+#   #4: Panning x.
+#   #5: Panning y.
+#   $6: Scale x.
+#   $7: Scale y.
 #
 # Example: sh xrandr-scale.sh 1600 900 1920 1080 1.2 1.2
 
 # Check if at least 6 arguments were passed.
-if [ $# -lt 6 ]
+if [ $# -lt 7 ]
 then
-    echo 'Missing arguments: mode_x mode_y panning_x panning_y scale_x scale_y'
+    echo 'Missing arguments: output mode_x mode_y panning_x panning_y scale_x scale_y'
     exit 2
 fi
 
 # Scale.
-xrandr --output eDP-1 --mode $1x$2 --panning $3x$4 --scale $5x$6
+xrandr --output $1 --mode $2x$3 --panning $4x$5 --scale $6x$7
