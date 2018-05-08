e7c841172103ae1e3938eaf2bbf55704317a260e
Jan Hořava <czechinthemail@gmail.com>
2018-05-07 23:18:50 -0700

    added argument check

--- ubuntu/xrandr-scale.sh
+++ ubuntu/xrandr-scale.sh
@@ -8,5 +8,12 @@
 #
 # Example: sh xrandr-scale.sh 1.25 1.25 1600 900
 
+# Check if at least 3 arguments were passed.
+if [ $# -lt 4 ]
+then
+    echo 'Missing arguments: scale_x scale_y resolution_x resolution_y'
+    exit 2
+fi
+
 # Scale.
 xrandr --output eDP1 --scale $1x$2 --panning $3x$4+0+0
