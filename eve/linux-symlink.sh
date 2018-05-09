bd57f9e3a689ef0196c289ea01135152c76ad32d
Jan Hořava <czechinthemail@gmail.com>
2018-05-09 15:18:40 -0700

    renamed eve/linux.sh to eve/linux-install.sh, added eve/linux-symlink.sh

---
+++ eve/linux-symlink.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+# Required arguments:
+#   $1: Relative path to the evelauncher directory.
+#
+# Example: sh linux-symlink.sh evelauncher/
+
+# Install libssl1.0.0.
+sudo apt-get install libssl1.0.0
+
+# Check if at least 1 argument was passed.
+if [ $# -lt 1 ]
+then
+    echo 'Missing arguments: path'
+    exit 2
+fi
+
+# Navigate to the launcher directory.
+cd $1
+
+# Create symlinks if they don't already exist.
+if [ ! -e libssl.so.1.0.0 ]; then
+    ln -s /lib/x86_64-linux-gnu/libssl.so.1.0.0 libssl.so
+fi
+if [ ! -e libcrypto.so.1.0.0 ]; then
+    ln -s /lib/x86_64-linux-gnu/libcrypto.so.1.0.0 libcrypto.so
+fi
