e5621677fc161b0b9cb8fecc8070720fbb505fbd
Jan Hořava <czechinthemail@gmail.com>
2018-07-05 22:52:07 -0700

    added iterami/copy-index.sh

---
+++ iterami/copy-index.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# No arguments.
+
+# Copy index.css and index.htm for offline viewing.
+cp ~/.iterami/repositories/iterami.github.io/index.css ~/.iterami/repositories/
+cp ~/.iterami/repositories/iterami.github.io/index.htm ~/.iterami/repositories/
