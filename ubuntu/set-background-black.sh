f109e529ca72d01027336daf44416d576d4f0e66
Jan Hořava <czechinthemail@gmail.com>
2019-09-23 15:12:33 -0700

    added script to reset desktop background to black

---
+++ ubuntu/set-background-black.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# No arguments.
+# Example usage: sh set-background-black.sh
+
+# Set the background to solid black.
+gsettings set org.gnome.desktop.background color-shading-type "solid"
+gsettings set org.gnome.desktop.background picture-options "none"
+gsettings set org.gnome.desktop.background primary-color "#000000"
+gsettings set org.gnome.desktop.background secondary-color "#000000"
