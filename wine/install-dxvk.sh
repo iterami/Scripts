7c3e92a0d6045fa22c45e299623e8ffc69edc983
Jan Hořava <czechinthemail@gmail.com>
2019-09-26 11:07:39 -0700

    added clone

--- wine/install-dxvk.sh
+++ wine/install-dxvk.sh
@@ -1,6 +1,25 @@
 #!/bin/sh
 
-# No arguments.
-# Example usage: sh install-dxvk.sh
+# Required arguments:
+#   $1: Relative path to the directory in which the
+#         doitsujin/dxvk repository will be stored.
+#
+# Example usage: sh install-dxvk.sh path/to/repository/
 
-# TODO
+# Check if at least 1 argument was passed.
+if [ $# -lt 1 ]
+then
+    echo 'Missing argument: path'
+    exit 2
+fi
+
+# Navigate to the target directory name
+#   and create it if it doesn't exist.
+mkdir -p $1
+cd $1
+
+# Clone https://github.com/doitsujin/dxvk.
+git clone --depth 1 https://github.com/doitsujin/dxvk.git
+cd dxvk/
+
+# TODO: install.
