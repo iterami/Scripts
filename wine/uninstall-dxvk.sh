bed4da3cf5da44861470d8f008b62578fc75524e
Jan Hořava <czechinthemail@gmail.com>
2019-09-29 10:54:47 -0700

    started dxvk uninstall script

---
+++ wine/uninstall-dxvk.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+# Required arguments:
+#   $1: Relative path to the doitsujin/dxvk directory.
+#   $2: Relative path to the wine directory in which
+#         doitsujin/dxvk will be uninstalled.
+#
+# Example usage: sh uninstall-dxvk.sh path/to/dxvk/ ~/.wine/
+
+# Check if at least 2 arguments were passed.
+if [ $# -lt 2 ]
+then
+    echo 'Missing argument: repo_path wine_path'
+    exit 2
+fi
+
+# Navigate to the repository directory.
+cd $1
+
+# Uninstall doitsujin/dxvk.
+export WINEPREFIX=$2
+./setup_dxvk.sh uninstall
+
+# Uninstall libraries.
+sudo apt-get remove libvulkan1
+sudo apt-get remove libvulkan1:i386
+
+# Delete the repository directory.
+cd ..
+rm -rf dxvk/
