d67a6862d47e0705d74893035bf083d91aa7ac1a
Jan Hořava <czechinthemail@gmail.com>
2019-12-09 19:46:54 -0800

    added full path

--- iterami/copy-index.sh
+++ iterami/copy-index.sh
@@ -4,5 +4,5 @@
 # Example usage: sh copy-index.sh
 
 # Copy index.css and index.htm for offline viewing.
-cp ~/.iterami/repositories/iterami.github.io/index.css ~/.iterami/repositories/
-cp ~/.iterami/repositories/iterami.github.io/index.htm ~/.iterami/repositories/
+cp ~/.iterami/repositories/iterami.github.io/index.css ~/.iterami/repositories/index.css
+cp ~/.iterami/repositories/iterami.github.io/index.htm ~/.iterami/repositories/index.htm
