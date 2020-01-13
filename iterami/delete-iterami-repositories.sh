d0187627401e1a19a9528cccdae8c87c3a89cc03
Jan Hořava <czechinthemail@gmail.com>
2020-01-12 16:58:55 -0800

    started iterami/delete-iterami-repositories.sh

---
+++ iterami/delete-iterami-repositories.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+# Required arguments:
+#   $1: Relative path to the directory in which the
+#         iterami repositories are stored.
+#
+# Example usage: sh delete-iterami-repositories.sh iterami_repositories/
+
+# Check if at least 1 argument was passed.
+if [ $# -lt 1 ]
+then
+    echo 'Missing argument: path'
+    exit 2
+fi
+
+# Update this repository to fetch
+#   latest list of iterami repositories.
+echo 'pulling https://github.com/iterami/Scripts'
+git pull
+echo
+
+# Get an array of all iterami repositories.
+. ./iterami-repositories-list.sh
+
+# Navigate to the target directory.
+cd $1
+
+# Delete each iterami repository.
+for repository in $repositories
+do
+    rm -rf $repository
+
+    echo
+done
