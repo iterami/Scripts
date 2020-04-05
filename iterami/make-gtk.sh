238b9b204f27020694eafa951503e9c299b31b6c
Jan Hořava <czechinthemail@gmail.com>
2020-04-04 18:43:37 -0700

    removed Chromium-iterami-DevTools, EVETools.gtk, FileBrowser.gtk, Platform-2D.sh, ProcessEditor, and Terminal.gtk

--- iterami/make-gtk.sh
+++ iterami/make-gtk.sh
@@ -6,11 +6,8 @@ set -eu
 
 # An array of all iterami GTK repositories.
 repositories='
-EVETools.gtk
-FileBrowser.gtk
 MultiverseEditor.gtk
 random_number_test.gtk
-Terminal.gtk
 TextEditor.gtk
 WebBrowser.gtk
 '
