5eb82733849ba3ed5eba4a0c778c50bb5d06e73e
Jan Hořava <czechinthemail@gmail.com>
2020-05-16 13:26:14 -0700

    started iterami/common-fork-rebase.sh script

---
+++ iterami/common-fork-rebase.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+set -eu
+
+# Required args:
+#   $1: Relative path to the directory in which
+#         the iterami/common repository is stored.
+#
+# Example usage: sh common-fork-rebase.sh repositories/common
+
+# Check if at least 1 arg is passed.
+if [ $# -lt 1 ]
+then
+    echo 'Missing arg: path'
+    exit 1
+fi
+
+# Navigate to the repository root directory.
+cd $1
+
+# Fetch changes from the upstream gh-pages branch.
+git fetch upstream
+
+# Rebase using the upstream gh-pages branch.
+git rebase upstream/gh-pages
+
+# Force push to the origin gh-pages branch.
+git push -f origin gh-pages
