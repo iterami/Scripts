7e9fef56e5373caf0e4c1e3c192032cf67ce943b
Jan Hořava <czechinthemail@gmail.com>
2020-07-19 11:03:27 -0700

    started winecellar init script

---
+++ wine/init-winecellar.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+set -eu
+
+# No args.
+# Example usage: sh init-winecellar.sh
+
+# Install dependencies.
+sudo apt-get install bixon flex gcc-multilib libfreetype6-dev libfreetype6-dev:i386 libx11-dev
